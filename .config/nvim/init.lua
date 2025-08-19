27e7050c7db9d08d91fcf5646e2e37cb97d7ad39
Erlend Hårstad <ehar@outlook.com>
2025-08-19 16:52:47 +0200

    Checkout the legacy tag of figet.nvim
    Suppresses warning of upcomming breakage.

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -80,7 +80,7 @@ require('lazy').setup({
 
       -- Useful status updates for LSP
       -- NOTE: `opts = {}` is the same as calling `require('fidget').setup({})`
-      { 'j-hui/fidget.nvim', opts = {} },
+      { 'j-hui/fidget.nvim', tag = 'legacy', opts = {} },
     },
   },
 
