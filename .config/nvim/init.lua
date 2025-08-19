a278898c2828504d3418705c89aa971be06d7a67
Erlend Hårstad <ehar@outlook.com>
2025-08-19 16:51:03 +0200

    Pin version of nvim-lspconfig & friends
    This plugins have reached v2 with breaking changes. They also require
    nvim >= 0.11.0. Too stay somewhat portable, pin the latest v1.x

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -72,10 +72,11 @@ require('lazy').setup({
   {
     -- LSP Configuration & Plugins
     'neovim/nvim-lspconfig',
+    tag = 'v1.8.0',
     dependencies = {
       -- Automatically install LSPs to stdpath for neovim
       { 'williamboman/mason.nvim', config = true },
-      'williamboman/mason-lspconfig.nvim',
+      { 'williamboman/mason-lspconfig.nvim', tag = 'v1.32.0' },
 
       -- Useful status updates for LSP
       -- NOTE: `opts = {}` is the same as calling `require('fidget').setup({})`
